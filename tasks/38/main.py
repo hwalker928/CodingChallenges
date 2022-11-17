86f7f7ba9d5ecfdcebc40a515308a813c2067438
Harry W <63236817+hwalker928@users.noreply.github.com>
2022-11-17 10:11:46 +0000

    ✅ Task 38

---
+++ tasks/38/main.py
@@ -0,0 +1,48 @@
+# Task 38 - Sing Along
+
+numbers = [{
+    "number": "TEN",
+    "plural": True
+}, {
+    "number": "NINE",
+    "plural": True
+}, {
+    "number": "EIGHT",
+    "plural": True
+}, {
+    "number": "SEVEN",
+    "plural": True
+}, {
+    "number": "SIX",
+    "plural": True
+}, {
+    "number": "FIVE",
+    "plural": True
+}, {
+    "number": "FOUR",
+    "plural": True
+}, {
+    "number": "THREE",
+    "plural": True
+}, {
+    "number": "TWO",
+    "plural": True
+}, {
+    "number": "ONE",
+    "plural": False
+}, {
+    "number": "NO",
+    "plural": True
+}]
+
+for number in numbers:
+    for i in range(0, 1):
+        if number['plural']:
+            print(f"{number['number']} green bottles hanging on the wall")
+            if not number['number'] == "NO":
+                print("And if one green bottle should accidentally fall,")
+                print("There'll be...\n")
+        else:
+            print(f"{number['number']} green bottle hanging on the wall")
+            print("And if one green bottle should accidentally fall,")
+            print("There'll be...\n")
