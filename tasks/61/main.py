28c5f905228bb562619258894ac6c4283be07788
Harry W <63236817+hwalker928@users.noreply.github.com>
2023-01-06 13:56:29 +0000

    ✅ Task 61

---
+++ tasks/61/main.py
@@ -0,0 +1,7 @@
+# Task 61 - Your name is...
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+form = input("Enter your form: ")
+
+print(f"Your name is {name}, you are {age} years old, and you are in form {form}.")
