1e7b62135b01054ad16797e639b3936993512641
Harry W <63236817+hwalker928@users.noreply.github.com>
2023-01-06 13:58:17 +0000

    🛠 Improve Task 61

--- tasks/61/main.py
+++ tasks/61/main.py
@@ -4,4 +4,6 @@ name = input("Enter your name: ")
 age = input("Enter your age: ")
 form = input("Enter your form: ")
 
-print(f"Your name is {name}, you are {age} years old, and you are in form {form}.")
+print(
+    f"Your name is {name}, you are {age} years old, and you are in form {form}."
+)
